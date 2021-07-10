0a1818faf783e51ccf99f00f207fae1d894b4bb4
Jose Guevara <josexx1234@gmail.com>
2021-07-10 08:50:06 -0400

    Add title to Home.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import Home from "./components/Home";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-200">
-      <div class="text-center">
-        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span class="block xl:inline">The Odin Project</span>
-        </h1>
-        <p class="mt-3 text-base text-gray-500 sm:mt-5 mx-auto md:mt-5 md:text-xl lg:mx-0">
-          All the principal projects of the curriculum The Odin Project buind
-          with react and tailwindcss
-        </p>
-      </div>
+    <div className="min-h-screen bg-gray-100">
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div class="text-center">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span class="block xl:inline">The Odin Project</span>
+        </h1>
+        <p class="mt-3 text-base text-gray-500 sm:mt-5 mx-auto md:mt-5 md:text-xl lg:mx-0">
+          All the principal projects of the curriculum The Odin Project buind
+          with react and tailwindcss
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
