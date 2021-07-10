673cf36a866b1bb700362a0ad123fa0b1beb023a
Jose Guevara <josexx1234@gmail.com>
2021-07-10 08:32:45 -0400

    First deploy to github page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="min-h-screen bg-gray-200">
+      <div class="text-center">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span class="block xl:inline">The Odin Project</span>
+        </h1>
+        <p class="mt-3 text-base text-gray-500 sm:mt-5 mx-auto md:mt-5 md:text-xl lg:mx-0">
+          All the principal projects of the curriculum The Odin Project buind
+          with react and tailwindcss
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
