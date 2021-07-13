edb4936d9eb434043d2e87214b4f4ef1fca87d8f
Jose Guevara <josexx1234@gmail.com>
2021-07-13 13:34:29 -0400

    feat(Finish basic requirements for library project): Termine todo lo que pide de basico  en the oding project falta refactorizar y hacer limpieza lo hare despues
    NOTA: Agregar alguna forma de guardado al proyecto

---
+++ src/icons/BookOpen.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function BookOpen() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="0 0 499.09 400"
+      className="h-6 w-6"
+    >
+      <g transform="translate(0 -652.36)">
+        <path
+          fill="#000"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="4"
+          d="M12.374 1006c150.75-13.875 302.39-14.408 455.02 0-3.444-95.638-10.307-189.32-7.778-288.37-93.751 3.259-147.24-75.783-227.58-13.081-90.906-64.653-90.903 18.62-222.62 18.385 6.286 95.682 3.846 189.18 2.952 283.07z"
+        ></path>
+        <path
+          fill="#fff"
+          d="M243.95 952.31c56.969-41.116 133.46 25.072 199.4 28.638l-.35-247.85c-104.27-6.8-141.23-84.275-200.82-14.496z"
+        ></path>
+        <path
+          fill="#fff"
+          d="M222.74 955.14c-56.97-41.12-133.46 25.07-199.4 28.63l.348-247.84c104.27-6.8 141.22-84.27 200.82-14.49z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default BookOpen;
--- src/pages/javascript/library/App.js
+++ src/pages/javascript/library/App.js
@@ -1,12 +1,21 @@
+// TODO: Optional - we haven’t learned any techniques for actually storing our data anywhere, so when the user refreshes the page, all of their books will disappear! If you want, you are capable of adding some persistence to this library app using the Web Storage API.
+/*localStorage (docs here) allows you to save data on the user’s computer. The downside here is that the data is ONLY accessible on the computer that it was created on. Even so, it’s pretty handy! Set up a function that saves the whole library array to localStorage every time a new book is created, and another function that looks for that array in localStorage when your app is first loaded. Additionally, here are a couple of quick tips to help you not get tripped up:
+Make sure your app doesn’t crash if the array you retrieve from localStorage isn’t there!
+localStorage uses JSON to send and store data, and when you retrieve the data, it will also be in JSON format. You will learn more about this language in a later lesson, but it doesn’t hurt to get your feet wet now. Keep in mind you cannot store functions in JSON, so you’ll have to figure out how to add methods back to your object properties once you fetch them. Good luck! */
+import AddDocument from "icons/AddDocument";
+import BookClose from "icons/BookClose";
+import BookOpen from "icons/BookOpen";
 import Trash from "icons/Trash";
 import React, { useState } from "react";
 
-function Book(title, author, review = "") {
+function Book(title = "", author = "", review = "") {
   this.title = title;
   this.author = author;
   this.review = review;
+  this.wasRead = false;
 }
 function App() {
+  const [newBook, setNewBook] = useState(new Book("", ""));
   const [books, setBooks] = useState([
     new Book(
       "Juegos del destino",
@@ -24,16 +33,125 @@ function App() {
       "Un buen libro para una lectura rapida"
     ),
   ]);
-
+  function handleNewBook(e) {
+    const book = { ...newBook };
+    book[e.target.name] = e.target.value;
+    setNewBook(book);
+  }
   return (
     <div className="bg-gray-400 min-w-full">
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 p-2 ">
+        <form className="bg-gray-100 container mx-auto flex flex-col rounded-lg shadow-xl mt-4 py-4 px-6 text-gray-800 justify-between h-64">
+          <div className="flex justify-between">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="title"
+            >
+              title
+            </label>
+            <input
+              className={`rounded-md ${
+                newBook.title ? null : "border-2 border-red-500"
+              }`}
+              type="text"
+              name="title"
+              value={newBook.title}
+              onChange={(e) => {
+                handleNewBook(e);
+              }}
+              required
+            />
+          </div>
+          <div className="flex justify-between">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="author"
+            >
+              Author
+            </label>
+
+            <input
+              className={`rounded-md ${
+                newBook.title ? null : "border-2 border-red-500"
+              }`}
+              type="text"
+              name="author"
+              value={newBook.author}
+              onChange={(e) => {
+                handleNewBook(e);
+              }}
+              required
+            />
+          </div>
+          <div className="min-w-full">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="review"
+            >
+              Review
+            </label>
+            <textarea
+              className={`w-full rounded-md ${
+                newBook.title ? null : "border-2 border-red-500"
+              }`}
+              type="text"
+              name="review"
+              value={newBook.review}
+              rows="3"
+              onChange={(e) => {
+                handleNewBook(e);
+              }}
+              required
+            />
+          </div>
+          <div className="flex justify-between">
+            <h4 className="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
+              Add Book
+            </h4>
+            <button
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                for (const key in newBook) {
+                  if (newBook[key] === "") {
+                    return;
+                  }
+                }
+                const newBooks = [...books, newBook];
+                setNewBook(new Book());
+                setBooks(newBooks);
+              }}
+              className="text-blue-700"
+            >
+              <AddDocument />
+            </button>
+          </div>
+        </form>
         {books.map((book, i) => {
           return (
-            <div className="bg-gray-100 container mx-auto flex flex-col rounded-lg shadow-xl mt-4 py-4 px-6 text-gray-800 justify-between">
-              <h3 className="font-semibold leading-tight text-lg truncate">
-                {book.title}
-              </h3>
+            <div className="bg-gray-100 container mx-auto flex flex-col rounded-lg shadow-xl mt-4 py-4 px-6 text-gray-800 justify-between h-64">
+              <div className="flex justify-between">
+                <h3 className="font-semibold leading-tight text-lg truncate">
+                  {book.title}
+                </h3>
+                <button
+                  className=""
+                  onClick={() => {
+                    const copyBooks = [...books];
+                    copyBooks[i].wasRead = !copyBooks[i].wasRead;
+                    setBooks(copyBooks);
+                  }}
+                >
+                  <div className="flex">
+                    {book.wasRead ? (
+                      <span>Reading...</span>
+                    ) : (
+                      <span>Read...</span>
+                    )}
+                    {book.wasRead ? <BookOpen /> : <BookClose />}
+                  </div>
+                </button>
+              </div>
               <p className="mt-2 h-12 max-h-12 overflow-hidden">
                 {book.review}
               </p>
@@ -48,7 +166,6 @@ function App() {
                       ...books.slice(0, i),
                       ...books.slice(i + 1),
                     ];
-                    console.log(newBooks);
                     setBooks(newBooks);
                   }}
                 >
