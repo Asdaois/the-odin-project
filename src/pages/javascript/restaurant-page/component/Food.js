ebfa88f1f661756a4f3941bd593cdd4ef3268c4f
Jose Guevara <josexx1234@gmail.com>
2021-07-17 16:59:21 -0400

    Add slide in effect to Food.js

--- src/pages/javascript/restaurant-page/component/Food.js
+++ src/pages/javascript/restaurant-page/component/Food.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
 export default function Food({ food, index }) {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useLayoutEffect(() => {
+    const topPosition = ref.current.getBoundingClientRect().top;
+    const botPosition = ref.current.getBoundingClientRect().bottom;
+    console.log(ref.current.getBoundingClientRect());
+
+    const onScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight;
+      const isNotScrolledPast = window.scrollY < botPosition;
+      if (topPosition < scrollPosition && isNotScrolledPast) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <div>
+    <div
+      ref={ref}
+      className="flex border-2 rounded-lg border-gray-400 bg-gray-200 text-gray-700 w-1/2 self-center"
+      key={food.name.toLowerCase()}
+    >
       <div
-        className="flex border-2 rounded-lg border-gray-400 bg-gray-200 text-gray-700 w-1/2 self-center"
-        key={food.name.toLowerCase()}
+        className={`flex flex-col flex-none w-1/2 ${
+          index % 2 === 0 ? "order-1" : null
+        }`}
       >
-        <div
-          className={`flex flex-col flex-none w-1/2 ${
-            index % 2 === 0 ? "order-1" : null
+        <h3 className="text-2xl h-1/4 text-center self-center pt-3 text-blue-500">
+          {food.name}
+        </h3>
+        <p className="h-full p-3">{food.description}</p>
+      </div>
+      <div className="flex-none w-1/2 object-contain rounded-lg">
+        <img
+          src={food.image}
+          alt=""
+          className={`transform transition-all duration-500 opacity-0  ${
+            show
+              ? "opacity-100 translate-x-0"
+              : index % 2
+              ? "translate-x-1/3"
+              : "-translate-x-1/3"
           }`}
-        >
-          <h3 className="text-2xl h-1/4 text-center self-center pt-3 text-blue-500">
-            {food.name}
-          </h3>
-          <p className="h-full p-3">{food.description}</p>
-        </div>
-        <div className="flex-none w-1/2 object-contain rounded-lg">
-          <img src={food.image} alt="" />
-        </div>
+        />
       </div>
     </div>
   );
--- src/pages/javascript/restaurant-page/component/Menu.js
+++ src/pages/javascript/restaurant-page/component/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { restaurantData } from "../data";
+import Food from "./Food";
 
 export default function Menu() {
   return (
@@ -7,32 +8,7 @@ export default function Menu() {
       <h1 className="text-4xl text-red-500 mb-4">Menu</h1>
       <div className="flex flex-col gap-2 justify-center">
         {restaurantData.menu.map((food, i) => {
-          return (
-            <div
-              className="flex border-2 rounded-lg border-gray-400 bg-gray-200 text-gray-700 w-1/2 self-center"
-              key={food.name.toLowerCase()}
-            >
-              <div
-                className={`flex flex-col flex-none w-1/2 ${
-                  i % 2 === 0 ? "order-1" : null
-                }`}
-              >
-                <h3 className="text-2xl h-1/4 text-center self-center pt-3 text-blue-500">
-                  {food.name}
-                </h3>
-                <p className="h-full p-3">{food.description}</p>
-              </div>
-              <div className="flex-none w-1/2 object-contain rounded-lg">
-                <img
-                  src={food.image}
-                  alt=""
-                  onScrollCapture={(e) => {
-                    console.log(e);
-                  }}
-                />
-              </div>
-            </div>
-          );
+          return <Food food={food} index={i} />;
         })}
       </div>
     </div>
